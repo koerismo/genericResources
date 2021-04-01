70f05c0fd4e1fd2a24befd139d907828e9e5d070
Koerismo <56563658+koerismo@users.noreply.github.com>
2021-04-01 11:00:48 -0400

    Update fill.js

--- bee2pkg/fill.js
+++ bee2pkg/fill.js
@@ -7,64 +7,76 @@ const editoritems_template = `
 		{
 		"SubType"
 			{
-			"Name"	"{ITEM_NAME}"
-			"Model"
+				"Name"				"{ITEM_NAME}"
+				"Model" { "ModelName"		"turret.3ds" }
+				"Palette"
 				{
-				"ModelName"	"turret.3ds"
+					"Tooltip"		"{ITEM_NAME}"
+					"Image"			"palette/clean/{ITEM_ID}.png"
+					"Position"		"4 2 0"
 				}
-			"Palette"
+				"Sounds"
 				{
-				"Tooltip"	"{ITEM_NAME}"
-				"Image"	"palette/clean/{ITEM_ID}.png"
-				"Position"	"4 2 0"
+				"SOUND_CREATED"			"P2Editor.PlaceOther"
+				"SOUND_EDITING_ACTIVATE"	"P2Editor.ExpandOther"
+				"SOUND_EDITING_DEACTIVATE"	"P2Editor.CollapseOther"
+				"SOUND_DELETED"			"P2Editor.RemoveOther"
 				}
-			"Sounds"
+				"Animations"
 				{
-				"SOUND_CREATED"	"P2Editor.PlaceOther"
-				"SOUND_EDITING_ACTIVATE" "P2Editor.ExpandOther"
-				"SOUND_EDITING_DEACTIVATE" "P2Editor.CollapseOther"
-				"SOUND_DELETED"	"P2Editor.RemoveOther"
-				...
-				}
-			"Animations"
-				{
-				"ANIM_IDLE"							"0"
-				"ANIM_EDITING_ACTIVATE"				"1"
-				"ANIM_EDITING_DEACTIVATE"			"2"
+					"ANIM_IDLE"			"0"
+					"ANIM_EDITING_ACTIVATE"		"1"
+					"ANIM_EDITING_DEACTIVATE"	"2"
 				...
 				}
 			}
 		}
-		"MovementHandle"		"HANDLE_4_DIRECTIONS"
-		"InvalidSurface" 		"WALL FLOOR CEILING"
-		"DesiredFacing"			"DESIRES_ANYTHING"
-		"CanAnchorOnGoo"		"0"
-		"CanAnchorOnBarriers" 	"0"
+		"MovementHandle"	"HANDLE_4_DIRECTIONS"
+		"InvalidSurface" 	"WALL FLOOR CEILING"
+		"DesiredFacing"		"DESIRES_ANYTHING"
+		"CanAnchorOnGoo"	"0"
+		"CanAnchorOnBarriers"	"0"
 	}
-"Properties"
+	"Properties" {}
+	"Exporting"
 	{
-	"PropertyName"
+		"Instances"
 		{
-		"DefaultValue"	"0"
-		"Index"	"1"
-		}
-	...
-	}
-"Exporting"
-	{
-	"Instances"
-		{
-		"0"
+			"0"
 			{
-			"Name" "instances/BEE2/clean/{ITEM_ID}.vmf"
-			"EntityCount"	"4"
-			"BrushCount"	"4" 
-			"BrushSideCount"	"24"
+				"Name" "instances/BEE2/clean/{ITEM_ID}.vmf"
+				"EntityCount"	"4"
+				"BrushCount"	"4" 
+				"BrushSideCount"	"24"
 			}
-		...
 		}
 	"TargetName"	"name"
 	"Offset"	"64 64 64"
+	}
+}
+`
+
+const properties_template = `
+"Properties" {
+	"Authors" ""
+	"Description" "{ITEM_DESC}"
+	"Icon" "clean/{ITEM_ID}.png"
+	{
+		"0" "clean/{ITEM_ID}.png"
+	}
+}
+`
+
+const info_item_template = `
+"Item"
+{
+	"ID"  "{ITEM_ID}"
+	"Version"
+	{
+		"Styles"
+		{
+			"BEE2_CLEAN" "{ITEM_ID}"
+		}
 	}
 }
 `
